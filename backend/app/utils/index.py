4cd8686e3d3e4d31a601e4dca00c35b71edaab79
Juan José Mata <juanjo.mata@gmail.com>
2023-11-23 15:37:57 +0100

    Phonenix and Gmail reading hack

--- backend/app/utils/index.py
+++ backend/app/utils/index.py
@@ -1,28 +1,69 @@
 import logging
 import os
 
+from phoenix.trace.llama_index import (
+    OpenInferenceTraceCallbackHandler,
+)
 from llama_index import (
     SimpleDirectoryReader,
     StorageContext,
     VectorStoreIndex,
-    load_index_from_storage,
     ServiceContext,
+    LLMPredictor,
+    OpenAIEmbedding,
+    load_index_from_storage,
+    download_loader
 )
+from llama_index.callbacks import CallbackManager
 from llama_index.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+import phoenix as px
 
+session = px.launch_app()
+
+# Once you have started a Phoenix server, you can start your LlamaIndex application with the `OpenInferenceTraceCallback` as a callback. To do this, you will have to add the callback to the initialization of your LlamaIndex application:
+from phoenix.trace.llama_index import (
+    OpenInferenceTraceCallbackHandler,
+)
 
 STORAGE_DIR = "./storage"  # directory to cache the generated index
 DATA_DIR = "./data"  # directory containing the documents to index
 
+# Initialize the callback handler
+callback_handler = OpenInferenceTraceCallbackHandler()
+
+# LlamaIndex application initialization may vary
+# depending on your application
 service_context = ServiceContext.from_defaults(
-    llm=OpenAI(model="gpt-3.5-turbo")
+#    llm_predictor=LLMPredictor(llm=ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key="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",temperature=0)),
+    llm=OpenAI(model="gpt-3.5-turbo"),
+    embed_model=OpenAIEmbedding(model="text-embedding-ada-002"),
+    callback_manager=CallbackManager(handlers=[callback_handler]),
 )
+
+# View the traces in the Phoenix UI
+px.active_session().url
 
 def get_index():
     logger = logging.getLogger("uvicorn")
     # check if storage already exists
     if not os.path.exists(STORAGE_DIR):
         logger.info("Creating new index")
+
+        if os.path.exists("./credentials.json"):
+            # Grab all emails from the last week
+            logger.info("Trying to load Gmail messages")
+            GmailReader = download_loader('GmailReader')
+            loader = GmailReader(query="newer_than:1d", max_results=100, results_per_page=100, service=None)
+            documents = loader.load_data()
+            logger.info("Grabbed {} emails".format(len(documents)))
+            index = VectorStoreIndex.from_documents(documents,service_context=service_context)
+            # store it for later
+            index.storage_context.persist(STORAGE_DIR)
+            logger.info("Loaded {} emails".format(len(documents)))
+        else:
+            logger.info("No credentials.json file present.")
+
         # load the documents and create the index
         documents = SimpleDirectoryReader(DATA_DIR).load_data()
         index = VectorStoreIndex.from_documents(documents,service_context=service_context)
